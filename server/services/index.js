a15ac026ef8266e06656846ac5923df2ff4ccf3d
daniel <danielrubensdaniel@gmail.com>
2023-04-16 10:40:36 -0300

    refactor: transcription service

--- server/services/index.js
+++ server/services/index.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const speech = require('@google-cloud/speech');
 
-const encoding = 'MP3';
-const sampleRateHertz = 16000;
-const languageCode = 'en-US';
-const filename = './audios/classicmodels.mp3';
-
-const config = { encoding, sampleRateHertz, languageCode };
-const audio = { content: fs.readFileSync(filename).toString('base64') };
-const request = { config, audio };
-
 const getTranscription = async () => {
+  const encoding = 'MP3';
+  const sampleRateHertz = 16000;
+  const languageCode = 'en-US';
+
+  // const options = { 1: './audios/classicmodels.mp3', 2: './audios/johnny-cash.mp3' };
+  // const filename = options[id];
+  const filename = './audios/classicmodels.mp3';
+
+  const config = { encoding, sampleRateHertz, languageCode };
+  const audio = { content: fs.readFileSync(filename).toString('base64') };
+  const request = { config, audio };
+
   const client = new speech.SpeechClient();
   const [response] = await client.recognize(request);
   const transcription = response.results
